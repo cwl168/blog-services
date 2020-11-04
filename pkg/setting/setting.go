6196832135501fc353d4b6c8077f3c22cd2ace11
weilin.cao <weilin.cao@langlive.com>
2020-11-04 18:48:01 +0800

    first commit

---
+++ pkg/setting/setting.go
@@ -0,0 +1,40 @@
+package setting
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Setting struct {
+	vp *viper.Viper
+}
+// 读取config.yaml 配置文件
+func NewSetting(configs ...string) (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	for _, config := range configs {
+		if config != "" {
+			vp.AddConfigPath(config)
+		}
+	}
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Setting{vp}
+	s.WatchSettingChange()
+	return s, nil
+}
+
+func (s *Setting) WatchSettingChange() {
+	go func() {
+		//开始监听
+		s.vp.WatchConfig()
+		//设置监听回调函数
+		s.vp.OnConfigChange(func(in fsnotify.Event) {
+			_ = s.ReloadAllSection()
+		})
+	}()
+}
